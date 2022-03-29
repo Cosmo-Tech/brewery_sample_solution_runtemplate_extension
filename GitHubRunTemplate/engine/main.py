5721d70940fd2852fe540af14eabac85d36a281d
Vincent Carluer <vcarluer@gmail.com>
2022-03-29 18:35:48 +0200

    investment simulation example

--- GitHubRunTemplate/engine/main.py
+++ GitHubRunTemplate/engine/main.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print("Hello Cosmo Extension!")
+from investment_simulator.portfolios import growth_simulation
+
+asset_weights = [0.5, 0.5]
+asset_returns = [0.1, 0.1]
+covariance = [[1.0, 0.0], [0.0, 1.0]]
+steps = 10
+investment_goal = 2.5
+
+result = growth_simulation(
+        asset_weightings= asset_weights,
+        annual_returns=asset_returns,
+        covariance=covariance,
+        steps=steps,
+        investment_goal=investment_goal,
+    )
+print(result)
